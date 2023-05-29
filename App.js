e6f4481576490f784167f6547c21a76755a53668
edineipinho <edneyrock@gmail.com>
2023-05-28 21:01:09 -0300

    :sparkles: feat: Header

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
+  const image = require('./resources/task.jpg');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={image} style={styles.image} >
+        <View style={styles.coverView}>
+          <Text style={styles.textHeader}>Task List</Text>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  image: {
+    resizeMode: 'cover',
+    width: '100%',
+    height: 100,
+  },
+
+  coverView: {
+    display: 'flex',
+    flexDirection: 'row',
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    height: 100,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  textHeader: {
+    display: 'flex',
+    color: '#fff',
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 4,
+  },
+})
